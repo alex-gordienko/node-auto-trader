67474fa2c252a4c9ee5991ef7350df06d604f93e
Alex Gordienko <alexoid1999@gmail.com>
2024-09-04 10:04:19 +0200

    fix getting balance for waves

--- src/services/WavesWallet.service.ts
+++ src/services/WavesWallet.service.ts
@@ -1,10 +1,11 @@
 import {
-  nodeInteraction,
+  // nodeInteraction,
   broadcast,
   transfer,
   WithId,
   WithProofs,
 } from "@waves/waves-transactions";
+import https from "https";
 import { TransferTransaction } from "@waves/ts-types";
 import { log, Colors } from "../utils/colored-console";
 import repeatEvent from "../utils/timer";
@@ -12,9 +13,13 @@ import DigitalOceanStorageService from "./DigitalOcean.storage.service";
 import { walletAmountStatistic } from "../utils/walletAmountStatistic";
 import cryptoConfig from "../config/crypto.config";
 import { CryptoBase } from "../types/basic.types";
+import axios from "axios";
+
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 class WavesWalletService {
-  private readonly infuraAPIkey = cryptoConfig.infuraApiKey;
   private readonly wavesWallet = cryptoConfig.wavesWallet;
   private readonly wavesPrivateKey = cryptoConfig.wavesPrivateKey;
 
@@ -72,7 +77,10 @@ class WavesWalletService {
 
   public getBalance = async (): Promise<number> => {
     try {
-      const balance = await nodeInteraction.balance(this.wavesWallet, this.wavesNodeUrl);
+      const response = await axios.get(`${this.wavesNodeUrl}/addresses/balance/${this.wavesWallet}`, {
+        httpsAgent: agent,
+      });
+      const balance = response.data.balance;
 
       return balance / Math.pow(10, 8);
     } catch (error) {
